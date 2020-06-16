05bb75b36239594694349ca2650f9a6968d96f5d
Alexander Yang <43522595+ayang015seas@users.noreply.github.com>
2020-06-16 11:05:00 -0500

    new file

--- rec.py
+++ rec.py
@@ -5,24 +5,30 @@ from flask import Flask
 from prometheus_client import start_http_server, Summary
 import random
 import time
-
 from prometheus_client import Counter
+from prometheus_client import Gauge
+
+# create new prometheus counter and gauge 
 c = Counter('my_failures', 'Description of counter')
+g = Gauge('decimal_aggregate', 'Aggregator')
 
 
+# connect to rabbitMQ server 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-rabbits = 0
 
+rabbits = 0.0
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    decimal = float(body)
     global rabbits
     c.inc()
-    rabbits += 1
+    g.set(rabbits)
+    rabbits += decimal
 
 
 channel.basic_consume(
@@ -48,22 +54,4 @@ if __name__ == '__main__':
         process_request(random.random())
 
 
-# app = Flask(__name__)
-# @app.route('/')
-# def hello_world():
-# 	return "Hello world!"
 
-print("Flask app up!")
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-
-# channel.start_consuming()
-
-# try:
-# 	_thread.start_new_thread(channel.start_consuming())
-# except:
-# 	print("Error, thread fail")
-# channel.start_consuming()
-
-
-print("hit bottom")
--- send.py
+++ send.py
@@ -1,4 +1,8 @@
 import pika
+from random import seed
+from random import random
+
+seed(1)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -7,7 +11,7 @@ channel.queue_declare(queue='hello')
 
 channel.basic_publish(exchange='',
 	routing_key='hello',
-	body='hello bud!'
+	body= str(random())
 	)
 
 print(" Sent Hello World")
