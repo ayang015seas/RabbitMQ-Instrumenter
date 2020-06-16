4ea90937af1df3aa3ebd93632698ccb4932bb782
Alexander Yang <43522595+ayang015seas@users.noreply.github.com>
2020-06-16 09:45:12 -0500

    new init commit

---
+++ receive.js
@@ -0,0 +1,41 @@
+var amqp = require('amqplib/callback_api');
+var express = require('express');
+
+var counter = 0;
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function(msg) {
+            counter++;
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
+
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+    console.log("Message Number " + counter);
+    res.send('Hello World!');
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
